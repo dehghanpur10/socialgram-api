c43ca5fa27926caaa8183fd8f1dc7b166eed705c
Mohammad Dehghanpour <m.dehghanpour10@gmail.com>
2021-10-19 13:19:24 +0330

    apply evn for port

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ SHELL := /bin/bash
 .DEFAULT_GOAL := build
 
 .EXPORT_ALL_VARIABLES:
-SERVER_PORT ?= 8080
+SERVER_PORT ?= :8000
 
 build:
 	go build -o main.exe main.go
--- main.go
+++ main.go
@@ -3,16 +3,17 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"socialgram/lib"
 	"socialgram/routes"
 )
 
-func main(){
+func main() {
 	fmt.Println("server run ...")
 
 	router := routes.Init()
 	http.Handle("/api/v1/", router)
 
-	err := http.ListenAndServe(":8080", router)
+	err := http.ListenAndServe(lib.SERVER_PORT, router)
 	if err != nil {
 		fmt.Println("main - problem in run server - error: ", err)
 	}
