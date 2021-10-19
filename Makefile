25ab10e48c3af526e0cf745b2f5399775be3b466
Mohammad Dehghanpour <m.dehghanpour10@gmail.com>
2021-10-20 00:45:20 +0330

    add env for connect to mySQL

--- Makefile
+++ Makefile
@@ -3,6 +3,11 @@ SHELL := /bin/bash
 
 .EXPORT_ALL_VARIABLES:
 SERVER_PORT ?= :8000
+DB_USER ?= mohammad
+DB_PASSWORD ?= 173946285
+DB_HOST ?= localhost
+DB_PORT ?= 3306
+DB_NAME ?= socialgram
 
 build:
 	go build -o main.exe main.go
--- lib/configs.go
+++ lib/configs.go
@@ -2,10 +2,20 @@ package lib
 
 import "os"
 
-var(
+var (
 	SERVER_PORT string
+	DB_USER     string
+	DB_PASSWORD string
+	DB_HOST     string
+	DB_PORT     string
+	DB_NAME     string
 )
 
 func init() {
 	SERVER_PORT = os.Getenv("SERVER_PORT")
+	DB_USER = os.Getenv("DB_USER")
+	DB_PASSWORD = os.Getenv("DB_PASSWORD")
+	DB_HOST = os.Getenv("DB_HOST")
+	DB_PORT = os.Getenv("DB_PORT")
+	DB_NAME = os.Getenv("DB_NAME")
 }
