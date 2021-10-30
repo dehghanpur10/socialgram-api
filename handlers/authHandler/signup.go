0e0957642093cb6a5d97ea7d46a0ea1171d8e166
Mohammad Dehghanpour <m.dehghanpour10@gmail.com>
2021-10-31 01:11:15 +0330

    start to implement signup function

--- handlers/authHandler/signup.go
+++ handlers/authHandler/signup.go
@@ -1,8 +1,12 @@
 package authHandler
 
 import (
+	"fmt"
+	"github.com/go-playground/validator/v10"
+	"io/ioutil"
 	"net/http"
 	"socialgram/lib"
+	"socialgram/models"
 )
 
 func SignUpHandler(w http.ResponseWriter, r *http.Request) {
@@ -10,4 +14,36 @@ func SignUpHandler(w http.ResponseWriter, r *http.Request) {
 		lib.HttpOptionsResponseHeaders(w)
 		return
 	}
+
+	lib.InitLog(r)
+
+	_, err := lib.GetDatabase()
+	if err != nil {
+		fmt.Println("SignUpHandler-GetDatabase error:", err)
+		lib.HttpError500(w)
+		return
+	}
+
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println("SignUpHandler-ioutil.ReadAll error:", err)
+		lib.HttpError400(w, err.Error())
+		return
+	}
+
+	userInput, err := models.ParsUserInputFrom(reqBody)
+	if err != nil {
+		fmt.Println("SignUpHandler-ParseWorkbenchInputFrom error:", err)
+		lib.HttpError400(w, err.Error())
+		return
+	}
+
+	validate := validator.New()
+	err = validate.Struct(userInput)
+	if err != nil {
+		fmt.Println("SignUpHandler error: ",err)
+		lib.HttpError400(w, err.Error())
+		return
+	}
+
 }
